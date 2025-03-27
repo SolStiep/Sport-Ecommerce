7f942bf9edd8ddceed94ffc121ad90a95cf3d932
SolStiep <solstiep.puan@gmail.com>
2025-03-27 21:59:52 +0100

    Tailwind and css configuration

--- e-commerce-frontend/src/router/index.tsx
+++ e-commerce-frontend/src/router/index.tsx
@@ -1,10 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import { HomePage } from "@/pages/Home/HomePage";
+import { AboutPage } from "@/pages/About/AboutPage";
+import { ContactPage } from "@/pages/Contact/ContactPage";
 // import { ProductDetailsPage } from "@/pages/ProductDetails/ProductDetailsPage";
 // import { ConfiguratorPage } from "@/pages/Configurator/ConfiguratorPage";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-//   { path: "/products/:productId", element: <ProductDetailsPage /> },
-//   { path: "/configurator/:productId", element: <ConfiguratorPage /> },
+    { path: "/", element: <HomePage /> },
+    //   { path: "/products/:productId", element: <ProductDetailsPage /> },
+    //   { path: "/configurator/:productId", element: <ConfiguratorPage /> },
+    { path: "/about", element: <AboutPage /> },
+    { path: "/contact", element: <ContactPage /> },
 ]);
